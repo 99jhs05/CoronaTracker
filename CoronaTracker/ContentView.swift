b82facf3c70f66dcfde0fff9bd323abcf4ed5c41
Jay Shin <08jhs05@gmail.com>
2020-03-09 19:51:39 -0400

    Initial Commit

---
+++ CoronaTracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CoronaTracker
+//
+//  Created by Jae Ho Shin on 2020-03-09.
+//  Copyright © 2020 Jae Ho Shin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
